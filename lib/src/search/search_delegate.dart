f79dfcc2cd068e744eb5c17bf0c0a692336a6959
Cesar Ramiro González López <Lopez.Cesar25@hotmail.com>
2020-12-04 17:38:18 -0600

    Ejemplo de una busqueda

--- lib/src/search/search_delegate.dart
+++ lib/src/search/search_delegate.dart
@@ -1,6 +1,23 @@
 import 'package:flutter/material.dart';
 
 class DataSearch extends SearchDelegate {
+  String seleccion = '';
+
+  final peliculas = [
+    'Spiderman',
+    'Aquaman',
+    'Batman',
+    'Shazam!',
+    'Ironman',
+    'Capitan America',
+    'Superman',
+    'Ironman 2',
+    'Ironman 3',
+    'Ironman 4'
+  ];
+
+  final peliculasRecientes = ['Spiderman', 'Capitan America'];
+
   @override
   List<Widget> buildActions(BuildContext context) {
     // Las acciones de nuestro AppBar
@@ -32,12 +49,46 @@ class DataSearch extends SearchDelegate {
   @override
   Widget buildResults(BuildContext context) {
     // Crea los resultados que vamos a mostrar
-    return Container();
+    return Center(
+      child: Container(
+        height: 100.0,
+        width: 100.0,
+        color: Colors.blueAccent,
+        child: Center(
+          child: Text(
+            seleccion,
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
     // Son las sugerencias que aparecen cuando la persona escribe
-    return Container();
+    final listaSugerida = (query.isEmpty)
+        ? peliculasRecientes
+        : peliculas
+            .where((element) =>
+                element.toLowerCase().startsWith(query.toLowerCase()))
+            .toList();
+
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return ListTile(
+          leading: Icon(Icons.movie),
+          title: Text(listaSugerida[index]),
+          onTap: () {
+            seleccion = listaSugerida[index];
+            showResults(context);
+          },
+        );
+      },
+      itemCount: listaSugerida.length,
+    );
   }
 }
